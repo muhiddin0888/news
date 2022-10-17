22758bcfe234a523c4104a05b5fa43bfe1c0c161
muhiddin0888 <djmixmikemonro@gmail.com>
2022-10-17 14:22:58 +0500

    some changes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "News",
           ),
           leading: IconButton(
